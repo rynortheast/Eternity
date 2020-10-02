0bc5cd10ced44472399e1c91139fcb97a5ae0a72
rynortheast <rynortheast@yandex.ru>
2020-10-02 15:51:21 +0300

    add: new task 4.8 - 8.2

--- lab_works/lab_task_1-8.py
+++ lab_works/lab_task_1-8.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 # the core file used to start laboratory task
 def launch(lesson, number):
@@ -13,6 +14,58 @@ def launch(lesson, number):
     elif lesson == 1 and number == 3:
         message = input('Обратный попугай, повторяй за мной: ')
         reverse_parrot(message.split(' '))
+    elif lesson == 2 and number == 1:
+        maze()
+    elif lesson == 2 and number == 2:
+        mail_registration()
+    elif lesson == 2 and number == 3:
+        schizophrenia()
+    elif lesson == 2 and number == 4:
+        da_or_net()
+    elif lesson == 3 and number == 1:
+        plus_or_minus()
+    elif lesson == 3 and number == 2:
+        calculator()
+    elif lesson == 3 and number == 3:
+        leap_years()
+    elif lesson == 3 and number == 4:
+        asunder()
+    elif lesson == 3 and number == 5:
+        layout()
+    elif lesson == 3 and number == 7:
+        beautiful_numb()
+    elif lesson == 3 and number == 9:
+        telegrams()
+    elif lesson == 4 and number == 1:
+        stroki()
+    elif lesson == 4 and number == 4:
+        vkPassword()
+    elif lesson == 4 and number == 5:
+        syracuse()
+    elif lesson == 4 and number == 8:
+        guessing_game()
+    elif lesson == 5 and number == 2:
+        circus()
+    elif lesson == 5 and number == 3:
+        nim()
+    elif lesson == 6 and number == 1:
+        reverse_report()
+    elif lesson == 6 and number == 2:
+        pyramid()
+    elif lesson == 7 and number == 1:
+        find_cat()
+    elif lesson == 7 and number == 2:
+        find_cat_v2()
+    elif lesson == 7 and number == 3:
+        find_cat_v3()
+    elif lesson == 8 and number == 1:
+        division_table()
+    elif lesson == 8 and number == 2:
+        haha_draw()
+    else:
+        print('\n- Задание не найдено (눈_눈)')
+
+    
 
 # task 1.1
 def horoscope(name, surname, animal, zodiac):
@@ -222,6 +275,8 @@ def telegrams():
     text = input('Один символ = 40 копеек. Напиши и посчитаем! - ')
     print('- Твое сообщение стоит нам: ', len(text) * 40 // 100, 'р. ', len(text) * 40 % 100, 'коп. ', sep='')
 
+# --------------------------------------------------------------------------------------- #
+
 # task 4.1
 def stroki():
     text = str()
@@ -270,7 +325,156 @@ def syracuse():
     print(int(number), end='')
     print('\nХодов до единицы -', counter)
     
+# task 4.8
+def guessing_game():
+    reply = str()
+    numb_1 = 1
+    numb_2 = 1000
+    counter = 0
 
+    print('Бинарная угадайка! Загадай число от 1 до 1000 (ಠ‿ಠ)\n')
+    while reply != '=':
+        step = random.randint(numb_1, numb_2)
+        reply = input('- Как насчет этого числа: ' + str(step) + ' - ')
+        if reply == '=':
+            numb_2 = step
+            break
+        elif reply == '>':
+            numb_2 = step - 1
+        elif reply == '<':
+            numb_1 = step + 1
+        else:
+            print('\n Оу, вы вели что-то непонятное! Попробуйте еще раз.')
+            continue
+        counter += 1
+    print('Ура! Вот и наш ответ:', numb_2, '\nУгадано за', counter, 'попыток!')
 
+# --------------------------------------------------------------------------------------- #
 
+# task 5.2
+def circus():
+    counter = 0
 
+    number = int(input('Какое кол-во камней необходимо? - '))
+    while number != 1:
+        if number % 2 == 1:
+            number -= 1
+        else:
+            number /= 2
+        counter += 1
+    print('- Необходимо сделать ' + str(counter) + ' действий!')
+
+# task 5.3
+def nim():
+    heap = list(range(2))
+    heap[0] = int(input('Кол-во камней в куче №1: '))
+    heap[1] = int(input('Кол-во камней в куче №2: '))
+    while heap[0] != 0 or heap[1] != 0:
+        select = int(input('\n- Выберите кучу: '))
+        count = int(input('- Сколько забираете? - '))
+        while heap[select - 1] - count < 0:
+            count = int(input('- Ошибка! Столько забрать невозможно.. Сколько? - '))
+        heap[select - 1] = heap[select - 1] - count
+        print('Куча №1 имеет - ' + str(heap[0]) + ' камней!')
+        print('Куча №2 имеет - ' + str(heap[1]) + ' камней!')
+    print('\nИгра окончена! Камней больше нет (ಥ﹏ಥ)')
+
+# task 5.4
+# task 5.5 - Стоит сделать эти задания, как появится возможность!
+
+# --------------------------------------------------------------------------------------- #
+
+# task 6.1
+def reverse_report():
+    times = int(input('Даешь отчёт! Стартовое значение? - '))
+    for step in range(times):
+        print('Осталось секунд - ' + str(times - step))
+        time.sleep(1)
+    print('Пуск!')
+
+# task 6.2
+def pyramid():
+    num = int(input('Боб-строитель на связи! Какова высота пиромиды? - '))
+    for step in range(num):
+        print(" " * (num - step), "*", "*" * 2 * step, " " * (num - step), sep='')
+
+# --------------------------------------------------------------------------------------- #
+
+# task 7.1
+def find_cat():
+    message = str()
+    print('Боже, нужно срочно найти кота! Зачем?! Без лишних вопросов (ノ°益°)ノ')
+    counter = int(input('- Сколько строк необходимо? - '))
+    for step in range(counter):
+        message += input('Ввод строки №' + str(step + 1) + ' - ')
+    if message.lower().find('кот') != -1:
+        print('- МЯУ')
+    else:
+        print('- ~ТИШИНА')
+
+#task 7.2
+def find_cat_v2():
+    message = str()
+    step = str()
+    counter = 1
+    catFind = -1
+    print('- Господи! Этот комок шерсти снова куда-то пропал.. НАЙДИ ЕГО! (ノ°益°)ノ')
+    while step != 'стоп':
+        message += step
+        step = input('Ввод строки №' + str(counter) + ' - ').lower()
+        if step.find('кот') != -1:
+            catFind = counter
+        counter += 1
+    if catFind != -1:
+        print('- Кот найден в строке №' + str(catFind) + ' - МЯУ')
+    else:
+        print('- ~ТИШИНА')
+
+#task 7.3
+def find_cat_v3():
+    message = str()
+    print('ААА, ХВАТИТ! Вот куда он снова делся! АА, ИЩИМ! (ノ°益°)ノ')
+    counter = int(input('- Сколько строк необходимо? - '))
+    for step in range(counter):
+        message += input('Ввод строки №' + str(step + 1) + ' - ')
+        if message.lower().find('кот') != -1:
+            print('- Ух, нашелся - МЯУ')
+            break
+        else:
+            print('- ~ТИШИНА')
+    return
+
+# --------------------------------------------------------------------------------------- #
+
+#task 8.1
+def division_table():
+    print('Программа для создания таблицы деления! (눈_눈)')
+    colums = int(input('- Введите кол-во столбцов: '))
+    lines = int(input('- Введите кол-во строк: '))
+
+    for step_one in range(lines):
+        print('')
+        for step_two in range(colums):
+            print(round((step_two + 1) / (step_one + 1), 1), end='   ')
+    print('\n\nНеобходимая таблицу создана. Поздравляю! (눈_눈)')
+
+#task 8.2
+def haha_draw():
+    print('Просто рисую прямоугольники разными символами! HELP ME! (눈_눈)')
+    heigh = int(input('- Введите высоту фигуры: '))
+    width = int(input('- Введите ширину фигуры: '))
+    sign = input('- Каким символом рисуем? - ')
+
+    for step in range(heigh):
+        if step == heigh - 1 or step == 0:
+            print(sign * width)
+        else:
+            print(sign, ' ' * (width - 2), sign, sep='')
+    
+    print('\nПрямоуголник из ' + sign + ' построен. Размер ' + str(width) + 'x' + str(heigh) + '. Спасибо!')
+
+lesson = int(input('- Номер урока: '))
+quest = int(input('- Номер задания: '))
+print('Поиск задания.. ᕕ( ᐛ )ᕗ \n')
+
+launch(lesson, quest)
